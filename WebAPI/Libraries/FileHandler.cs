5b29f65842f72f310b95e5deb6d224903782eb8b
Ayush Kumar <kumarayush2104@gmail.com>
2024-11-02 17:39:14 +0530

    WebAPI: Fixed file path while storing in the database

--- WebAPI/Libraries/FileHandler.cs
+++ WebAPI/Libraries/FileHandler.cs
@@ -3,14 +3,14 @@ namespace WebAPI.Libraries
     public static class FileHandler
     {
         private const string RootPath = "wwwroot";
-        private const string SchoolDataPath = $"{RootPath}/SchoolData";
-        private const string StudentDataPath = $"{RootPath}/StudentData";
-        private const string TeacherDataPath = $"{RootPath}/TeacherData";
+        private const string SchoolDataPath = $"SchoolData";
+        private const string StudentDataPath = $"StudentData";
+        private const string TeacherDataPath = $"TeacherData";
         public static string UpdatePrincipalProfilePicture(IFormFile file)
         {
             if (file.Length <= 0 || file == null) throw new NullReferenceException(nameof(file));
             string fileName = $"{SchoolDataPath}/principal{Path.GetExtension(file.FileName)}";
-            using FileStream fileStream = new(fileName, FileMode.Create);
+            using FileStream fileStream = new($"{RootPath}/{fileName}", FileMode.Create);
             file.CopyTo(fileStream);
             return fileName;
         }

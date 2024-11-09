69e5dfdfc44e8635bf28afb40d82836d049141ee
Ayush Kumar <kumarayush2104@gmail.com>
2024-11-09 16:12:51 +0530

    WebAPI: Added Enum Serialiser and fixed CORS policy

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,20 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddCors(policy => {
-    policy.AddPolicy("CorsPolicy", rules => {
+builder.Services.AddControllers().AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())); ;
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("CorsPolicy", rules =>
+    {
         rules.AllowAnyMethod();
         rules.AllowAnyOrigin();
+        rules.AllowAnyHeader();
     });
 });
 
